ff78da3b6c8abc4decb8b6eaf53ff2f5f2c16a8c
yo-yan <yooyan0512@gmail.com>
2021-01-13 22:26:15 +0900

    TypeScript 課題2 1/13

---
+++ src/types/UserType.ts
@@ -0,0 +1,19 @@
+import React from 'react'
+
+//課題２
+type MondaiType = {
+    id: number;
+    name: string;
+    url: string;
+    isStart: false;
+}
+
+let mondai: MondaiType = { id: 1, name: "hashimoto", url: "https://mondai.com", isStart: false }
+
+//EXTRA課題
+type ExtraType = {
+    id: number;
+    name: string;
+    url: string;
+    email: string;
+}
